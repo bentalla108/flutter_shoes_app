ce5b812883521c6bd4d676a746ee2b954e9373bc
bentalla108 <tallambaye108@gmail.com>
2023-08-23 20:15:01 +0000

    Flutter - Addind Cart Page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/global_variables.dart';
-import 'package:shop_app/product_cart.dart';
-import 'package:shop_app/products_details.dart';
+import 'package:shop_app/cart_page.dart';
+import 'package:shop_app/product_list.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,142 +10,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final List<String> filters = const ['All', 'adidas', 'Nike', 'Pumas'];
-  late String selectedFilter;
+  final List<Widget> page = const [ProductList(), CartPage()];
   int currentPage = 0;
 
   @override
-  void initState() {
-    super.initState();
-    selectedFilter = filters[0];
-  }
-
-  @override
   Widget build(BuildContext context) {
-    const border = OutlineInputBorder(
-      borderSide: BorderSide(
-        color: Color.fromRGBO(
-          225,
-          225,
-          225,
-          1,
-        ),
-        style: BorderStyle.solid,
-        width: 2,
-      ),
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(50),
-        bottomLeft: Radius.circular(50),
-      ),
-    );
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    'Shoes\nCollection',
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                ),
-                const Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.search,
-                      ),
-                      hintText: 'Search',
-                      border: border,
-                      focusedBorder: border,
-                      enabledBorder: border,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: filters.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(left: 8, right: 8),
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedFilter = filters[index];
-                        });
-                      },
-                      child: Chip(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 20,
-                          vertical: 15,
-                        ),
-                        backgroundColor: selectedFilter == filters[index]
-                            ? Theme.of(context).colorScheme.primary
-                            : const Color.fromRGBO(245, 247, 249, 1),
-                        side: const BorderSide(
-                          color: Color.fromRGBO(245, 247, 249, 1),
-                        ),
-                        label: Text(
-                          filters[index],
-                        ),
-                        labelStyle: const TextStyle(
-                          fontSize: 16,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  final product = products[index];
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(
-                        MaterialPageRoute<void>(
-                          builder: (BuildContext context) =>
-                              ProductDetail(product: product),
-                        ),
-                      );
-                    },
-                    child: ProductCart(
-                      price: products[index]['price'] as double,
-                      title: products[index]['title'] as String,
-                      urlImage: products[index]['imageUrl'] as String,
-                      backgroundColor: index.isEven
-                          ? const Color.fromRGBO(216, 240, 253, 1)
-                          : const Color.fromRGBO(245, 247, 249, 1),
-                    ),
-                  );
-                },
-              ),
-            ),
-            BottomNavigationBar(
-                currentIndex: currentPage,
-                onTap: (value) {
-                  setState(() {
-                    currentPage = value;
-                  });
-                },
-                items: const [
-                  BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.shopping_cart), label: ''),
-                ])
-          ],
-        ),
+      body: IndexedStack(
+        index: currentPage,
+        children: page,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        iconSize: 35,
+        currentIndex: currentPage,
+        onTap: (value) {
+          setState(() {
+            currentPage = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: ''),
+        ],
       ),
     );
   }
